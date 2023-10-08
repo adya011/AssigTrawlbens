b6b9b60433b953e1569251980d6fc7f25f573237
nandasamudera <samudera.nanda@gmail.com>
2023-10-09 00:58:09 +0700

    add NewsArticleViewModel, update adjust and bind NewsArticleFragment

---
+++ app/src/main/java/com/nanda/assigtrawlbens/feature/article/NewsArticleViewModel.kt
@@ -0,0 +1,10 @@
+package com.nanda.assigtrawlbens.feature.article
+
+import androidx.lifecycle.ViewModel
+import com.nanda.assigtrawlbens.domain.usecase.NewsUseCase
+
+class NewsArticleViewModel(
+    private val newsUseCase: NewsUseCase
+): ViewModel() {
+
+}
